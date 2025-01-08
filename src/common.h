48e8216e0f04fbb2444dcd09ba5aab76180929cc
Daniel Alp <daniel.alp.email@gmail.com>
2025-01-08 01:54:24 -0500

    delete everything

--- src/common.h
+++ src/common.h
@@ -1,4 +1,3 @@
 #pragma once
-#include <vector>
-#include <array>
 #include <cstdint>
+#include <array>
--- src/sudoku.cpp
+++
@@ -1,5 +0,0 @@
-#include "sudoku.h"
-
-void solve(std::array<int32_t, 81>& grid, std::array<uint16_t, 81>& candidates, int sq) {
-
-}
